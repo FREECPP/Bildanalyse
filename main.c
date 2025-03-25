99ddc9523df9963662bcba944596a36f55262c60
FREECPP <felix.rehm2000@gmail.com>
2025-03-25 20:49:28 +0100

    changed color into gray

--- main.c
+++ main.c
@@ -16,7 +16,27 @@ int main(void) {
         exit(1);
     }
     printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
-    stbi_write_jpg("/Users/felixrehm/CLionProjects/Bildanalyse/Rubiks1.jpg", width, height, channels, data, 100);
-    stbi_image_free(data);
+
+    // Convert the image to gray
+    size_t img_size = width * height * channels;
+    int gray_channels = channels == 4 ? 2 : 1;
+    size_t img_gray_size = width * height * gray_channels;
+
+    unsigned char *gray_img = malloc(img_gray_size);
+    if (gray_img == NULL) {
+        printf("Unable to allocate memory for the gray image\n");
+        exit(1);
+    }
+
+    for (unsigned char *p = data, *pg = gray_img;p != data + img_size; p += channels, pg += gray_channels) {
+        *pg = (u_int8_t)((*p + *(p + 1) + *(p + 2))/ 3.0);
+        if (channels ==4) {
+            *(pg + 1) = *(p + 3);
+        }
+    }
+    stbi_write_jpg("/Users/felixrehm/CLionProjects/Bildanalyse/sky_gray.jpg", width, height, gray_channels, gray_img, 100);
+
+
+    free(gray_img);
     return 0;
 }
