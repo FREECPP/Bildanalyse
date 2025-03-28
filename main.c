666f779c9ef62ec77328ea623222cc6177b4ca00
FREECPP <felix.rehm2000@gmail.com>
2025-03-28 22:46:48 +0100

    still not working - brings up memory sementation fault

--- main.c
+++ main.c
@@ -7,14 +7,10 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-void linear_scaling(size_t img_size, unsigned char* data,int channels) {
-    unsigned char* high_contrast = (unsigned char*)malloc(img_size);
-    if (high_contrast == NULL) {
-        printf("Es konnte kein Speicher für high_contrast allokiert werden");
-        exit(1);
-    }
+unsigned char* linear_scaling( unsigned char* high_contrast,size_t img_size, unsigned char* data,int channels) {
+
     u_int8_t smallest_brightness = 255, highest_brightness = 0;
-    unsigned char *ptr_brightest_pxl, *ptr_darkest_pixl; //Biedermansche ungereimtheit != unsigned char* ptr_brightest_pxl, ptr_darkest_pixl;
+    unsigned char *ptr_brightest_pxl = NULL, *ptr_darkest_pixl = NULL; //Biedermansche ungereimtheit != unsigned char* ptr_brightest_pxl, ptr_darkest_pixl;
     for (unsigned char* p = data; *p != *data + img_size; p+= channels) {
         u_int8_t brightness = (u_int8_t)0.299 * *p + 0.587 * *(p + 1) + 0.114 * *(p +2);
         if (brightness > highest_brightness) {
@@ -26,16 +22,18 @@ void linear_scaling(size_t img_size, unsigned char* data,int channels) {
             ptr_darkest_pixl = p;
         }
     }
-    //Für neuen speicherbereich machen - noch falsch
-    for (unsigned char* p = data; p !=  data + img_size; p += channels) {
+    if (ptr_brightest_pxl == NULL || ptr_darkest_pixl == NULL ) {
+        printf("Es wurde weder ein hellster Pixel noch ein dunkelster Pixel gefunden\n");
+        exit(1);
+    }
+    for (unsigned char* p = high_contrast; p !=  high_contrast + img_size; p += channels) {
         *p =(*p - *ptr_darkest_pixl) / (*ptr_brightest_pxl - *ptr_darkest_pixl) * 255;
         *(p+1) =(*(p+1) - *ptr_darkest_pixl) / (*ptr_brightest_pxl - *ptr_darkest_pixl) * 255;
         *(p+2) =(*(p+2) - *ptr_darkest_pixl) / (*ptr_brightest_pxl - *ptr_darkest_pixl) * 255;
     }
 
+    return high_contrast;
 
-    free(high_contrast); // Speicher wird wieder freigegeben
-    high_contrast = NULL; // Pointer wird wieder auf NULL gesetzt
 }
 
 int main(void) {
@@ -52,12 +50,22 @@ int main(void) {
     size_t img_size = width * height * channels;
     int gray_channels = channels == 4 ? 2 : 1;
     size_t img_gray_size = width * height * gray_channels;
-
+/*
     unsigned char *gray_img = malloc(img_gray_size);
     if (gray_img == NULL) {
         printf("Unable to allocate memory for the gray image\n");
         exit(1);
     }
+    */
+    unsigned char* high_contrast = malloc(img_size);
+    if (high_contrast == NULL) {
+        printf("Es konnte kein Speicher für high_contrast allokiert werden\n");
+        exit(1);
+    }
+    //erhöhe den Kontrast
+    unsigned char *high_contrast_return = linear_scaling(high_contrast,img_size,data,channels);
+    stbi_write_jpg("/Users/felixrehm/CLionProjects/Bildanalyse/high_contrast.jpg", width,height,channels,high_contrast_return,100);
+/*
     //mache das Bild schwarz weiß
     for (unsigned char *p = data, *pg = gray_img;p != data + img_size; p += channels, pg += gray_channels) {
         *pg = (u_int8_t)((*p + *(p + 1) + *(p + 2))/ 3.0);
@@ -66,9 +74,10 @@ int main(void) {
         }
     }
     stbi_write_jpg("/Users/felixrehm/CLionProjects/Bildanalyse/sky_gray.jpg", width, height, gray_channels, gray_img, 100);
-
-
-    free(gray_img);
-    gray_img = NULL;
+*/
+    free(high_contrast);
+    high_contrast = NULL;
+    //free(gray_img);
+    //gray_img = NULL;
     return 0;
 }
